17defc1cbd38d6be25d2a1b535dd9171a0e355df
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-17 23:25:22 +0500

    update code

--- Program.cs
+++ Program.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 
+// --------------------------------------------------------------------------------------
+
 /*
 A+B
 It will be necessary to calculate the sum of integers A and B.
 */
 
-string A = Console.ReadLine();
-int firstNumber = Convert.ToInt32(A);
+// string A = Console.ReadLine();
+// int firstNumber = Convert.ToInt32(A);
 
-string B = Console.ReadLine();
-int secondNumber = Convert.ToInt32(B);
+// string B = Console.ReadLine();
+// int secondNumber = Convert.ToInt32(B);
 
-Console.WriteLine(GetSumTwoNumbers(firstNumber,secondNumber));
+// Console.WriteLine(GetSumTwoNumbers(firstNumber,secondNumber));
 
-static int GetSumTwoNumbers(int firstNumber,int secondNumber)
-{
-    return firstNumber + secondNumber;
-}
+// static int GetSumTwoNumbers(int firstNumber,int secondNumber)
+// {
+//     return firstNumber + secondNumber;
+// }
+
+// --------------------------------------------------------------------------------------
