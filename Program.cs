f4cb3ea2fe3a745d92bbaa23e29c2b48dea7d85d
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-17 23:24:12 +0500

    A+B

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using System;
 
-Console.WriteLine("Hello");
+/*
+A+B
+It will be necessary to calculate the sum of integers A and B.
+*/
+
+string A = Console.ReadLine();
+int firstNumber = Convert.ToInt32(A);
+
+string B = Console.ReadLine();
+int secondNumber = Convert.ToInt32(B);
+
+Console.WriteLine(GetSumTwoNumbers(firstNumber,secondNumber));
+
+static int GetSumTwoNumbers(int firstNumber,int secondNumber)
+{
+    return firstNumber + secondNumber;
+}
